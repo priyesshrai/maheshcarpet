9cf5ca8917140cae0d79d654cf3255e878f6a45a
PriyeshRai369 <priyeshrai369@gmail.com>
2025-01-06 10:45:16 +0530

    Update export name.

--- src/app/collections/shaggy/[productId]/page.jsx
+++ src/app/collections/shaggy/[productId]/page.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Product from "./product";
+import Product from "./Product";
 
 export default async function Page({ params }) {
   const { productId } = await params;
