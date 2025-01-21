450936dc0b46a8bdba42ed43eaafa79bd5f8dc55
PriyeshRai369 <priyeshrai369@gmail.com>
2025-01-21 14:26:27 +0530

    update contact page add new address.

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,15 +1,51 @@
-'use client'
-import axios from 'axios'
-import React, { useEffect } from 'react'
 
 export default function page() {
 
-  useEffect(()=>{
-    axios.get("/api/contact")
-  },[])
   return (
-    <section className=''>
-      
+    <section className='section' style={{backgroundColor:"#fff", paddingBottom:"0"}}>
+      <div className="section-wraper">
+        <div className="section-wraper-box">
+          <div className="location-container">
+            <div className="location-heading">
+              <h5><strong>Varanasi Office (Headquaters)</strong></h5>
+            </div>
+            <p>Araji No 142, G.T. Road, Jagatpur
+              Varanasi, Uttar Pradesh - 221302</p>
+
+            <span>
+              <i class="hgi-stroke hgi-flip-phone" /> +91-9517292555
+            </span>
+          </div>
+        </div>
+
+        <div className="section-wraper-box">
+          <div className="location-container">
+            <div className="location-heading">
+              <h5><strong>Bhadhoi Office (Production)</strong></h5>
+            </div>
+            <p>Main Road, Maryadpatti
+              Bhadohi, Uttar Pradesh - 221401</p>
+
+            <span>
+              <i class="hgi-stroke hgi-flip-phone" /> +91-9918639000
+            </span>
+          </div>
+        </div>
+
+        <div className="section-wraper-box">
+          <div className="location-container">
+            <div className="location-heading">
+              <h5><strong>New Delhi Office (Marketing)</strong></h5>
+            </div>
+            <p>GK-1 C-7 3rd Floor Near M-Block Market New Delhi - 110048</p>
+
+            <span>
+              <i class="hgi-stroke hgi-flip-phone" /> +91-9415201178
+            </span>
+          </div>
+        </div>
+
+      </div>
     </section>
   )
 }
