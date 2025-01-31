7216a9fc1344371355547542ec39e8894985c4bd
PriyeshRai369 <priyeshrai369@gmail.com>
2025-01-31 14:16:21 +0530

    update contact.

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -2,7 +2,7 @@
 export default function page() {
 
   return (
-    <section className='section' style={{backgroundColor:"#fff", paddingBottom:"0"}}>
+    <section className='section' style={{ backgroundColor: "#fff", paddingBottom: "0" }}>
       <div className="section-wraper">
         <div className="section-wraper-box">
           <div className="location-container">
@@ -10,7 +10,9 @@ export default function page() {
               <h5><strong>Varanasi Office (Headquaters)</strong></h5>
             </div>
             <p>Araji No 142, G.T. Road, Jagatpur
-              Varanasi, Uttar Pradesh - 221302</p>
+              Varanasi.
+            </p>
+            <p>Uttar Pradesh - 221302</p>
 
             <span>
               <i class="hgi-stroke hgi-flip-phone" /> +91-9517292555
@@ -24,7 +26,8 @@ export default function page() {
               <h5><strong>Bhadhoi Office (Production)</strong></h5>
             </div>
             <p>Main Road, Maryadpatti
-              Bhadohi, Uttar Pradesh - 221401</p>
+              Bhadohi.</p>
+              <p>Uttar Pradesh - 221401</p>
 
             <span>
               <i class="hgi-stroke hgi-flip-phone" /> +91-9918639000
@@ -37,7 +40,8 @@ export default function page() {
             <div className="location-heading">
               <h5><strong>New Delhi Office (Marketing)</strong></h5>
             </div>
-            <p>GK-1 C-7 3rd Floor Near M-Block Market New Delhi - 110048</p>
+            <p>GK-1 C-7 3rd Floor Near M-Block Market.</p>
+            <p>New Delhi - 110048</p>
 
             <span>
               <i class="hgi-stroke hgi-flip-phone" /> +91-9415201178
